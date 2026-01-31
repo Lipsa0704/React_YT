47fd6bb3d8ade1639346f0b0bf7cbbe65ad5f0ca
Lipsa0704 <bhuyanlipsalin07@gmail.com>
2026-01-31 18:23:46 +0530

    commit to daily practice

---
+++ uiproject/src/components/section1/HeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-10'>
+            <h3 className='mb-7 text-4xl font-bold'>
+                Prospective <span className='text-gray-800'>customer</span> segmentation</h3>
+            <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, voluptatum quas distinctio mollitia commodi iure libero saepe omnis itaque? Dignissimos.
+            </p>
+    </div>
+  )
+}
+
+export default HeroText
---
+++ uiproject/src/components/section1/RightCardContent.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { MoveRight } from 'lucide-react';
+
+
+const RightCardContent = (props) => {
+  return (
+    <div  className='absolute top-0 left-0 h-full w-full p-10 flex flex-col justify-between'>
+
+            <h2 className='bg-white text-xl font-semibold rounded-full h-12 w-12 flex justify-center items-center'>{props.id+1}</h2>
+            <div>
+                <p className='text-lg  font-semibold leading-relaxed text-white mb-10'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, id explicabo aliquid error asperiores autem.</p>
+           
+
+            <div className='flex justify-between'>
+            <button style={{backgroundColor:props.color}} className='bg-blue-600 text-white font-medium px-8 py-3 rounded-full'>{props.tag}</button>
+
+           <button style={{backgroundColor:props.color}} className=' text-white font-medium px-3 py-2 rounded-full'> <MoveRight/> </button>
+            </div>
+             </div>
+        </div>
+  )
+}
+
+export default RightCardContent
